b5540ca8f3b8dd6b218bcb08731e8ebecfc1b81f
Eric Gao <754095810@qq.com>
2020-04-02 03:23:26 +0800

    test adding Student

--- src/test/java/com/erjiangao/tutorselectiontool/service/UserServiceTest.java
+++ src/test/java/com/erjiangao/tutorselectiontool/service/UserServiceTest.java
@@ -1,9 +1,11 @@
 package com.erjiangao.tutorselectiontool.service;
 
+import com.erjiangao.tutorselectiontool.entity.Student;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.annotation.Rollback;
 
 @SpringBootTest
@@ -11,11 +13,16 @@ import org.springframework.test.annotation.Rollback;
 @Rollback(value = false)
 public class UserServiceTest {
     @Autowired
-    private UserService userService;
+    private StudentService studentService;
+    @Autowired
+    private PasswordEncoder encoder;
 
     @Test
-    public void test_getStudentById() {
-        int id = 2;
-        log.debug(userService.getStudent(id).getName());
+    public void test_addStudent() {
+        Student student = new Student();
+        student.setName("WANG");
+        student.setPassword(encoder.encode("123456"));
+        student.setStudentIdNo("201702");
+        studentService.addStudent(student);
     }
 }
