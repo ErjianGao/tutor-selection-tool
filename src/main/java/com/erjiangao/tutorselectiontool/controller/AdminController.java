4bac397057cab967e0173049f0e8325ac7d497a4
Eric Gao <754095810@qq.com>
2020-04-10 18:09:35 +0800

    add admin service: add teacher, and course

--- src/main/java/com/erjiangao/tutorselectiontool/controller/AdminController.java
+++ src/main/java/com/erjiangao/tutorselectiontool/controller/AdminController.java
@@ -2,6 +2,11 @@ package com.erjiangao.tutorselectiontool.controller;
 
 import com.erjiangao.tutorselectiontool.component.ResponseComponent;
 import com.erjiangao.tutorselectiontool.entity.Admin;
+import com.erjiangao.tutorselectiontool.entity.Course;
+import com.erjiangao.tutorselectiontool.entity.Teacher;
+import com.erjiangao.tutorselectiontool.entity.User;
+import com.erjiangao.tutorselectiontool.service.CourseService;
+import com.erjiangao.tutorselectiontool.service.TeacherService;
 import com.erjiangao.tutorselectiontool.service.UserService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,8 +27,15 @@ public class AdminController {
     private UserService userService;
     @Autowired
     private PasswordEncoder encoder;
+    @Autowired
+    private TeacherService teacherService;
+    @Autowired
+    private CourseService courseService;
 
-    // 用户首页
+    /**
+     * 测试用，欢迎页
+     * @return 欢迎信息
+     */
     @GetMapping("")
     public Map welcome() {
         log.debug("{}", responseComponent.getUid());
@@ -31,6 +43,42 @@ public class AdminController {
         return Map.of("msg", "欢迎光临，管理员");
     }
 
+    /**
+     * 添加老师
+     * @param teacher
+     * @return
+     */
+    @PostMapping("/teachers")
+    public Map addTeacher(@RequestBody Teacher teacher) {
+        if (teacherService.getTeacher(teacher.getId()) != null) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "该老师已经存在，不必反复添加！");
+        }
+        // IdentityNo为必填内容
+        teacher.setPassword(encoder.encode(teacher.getIdentityNo()));
+        teacher.setRole(User.Role.TEACHER);
+        teacherService.addTeacher(teacher);
+        return Map.of("teacher", teacher);
+    }
+
+    /**
+     * 添加课程
+     * @param course 课程
+     * @return 课程
+     */
+    @PostMapping("/courses")
+    public Map addCourse(@RequestBody Course course) {
+        if (courseService.getCourse(course.getId()) != null) {
+            throw new ResponseStatusException((HttpStatus.BAD_REQUEST), "该课程已经存在，不必反复添加！");
+        }
+        courseService.addCourse(course);
+        return Map.of("course", course);
+    }
+
+    /**
+     * 管理员更新密码
+     * @param admin 管理员的旧密码，新密码，及确认密码
+     * @return 管理员
+     */
     @PatchMapping("/password")
     public Map updatePassword(@RequestBody Map<String, String> admin) {
         String oldPassword = admin.get("oldPassword");
