858a54605c8d2c5561badc4877ad9449c49167ad
Eric Gao <754095810@qq.com>
2020-04-28 02:38:58 +0800

    change "Authorization" to "authorization"

--- src/main/java/com/erjiangao/tutorselectiontool/component/MyToken.java
+++ src/main/java/com/erjiangao/tutorselectiontool/component/MyToken.java
@@ -7,8 +7,8 @@ import lombok.Data;
 @Data
 @AllArgsConstructor
 public class MyToken {
-    // 按照约定，以“Authorization”为键存放信息到Header中，这里使用常量，尽可能减少硬编码
-    public static final String AUTHORIZATION = "Authorization";
+    // 按照约定，以“authorization”为键存放信息到Header中，这里使用常量，尽可能减少硬编码
+    public static final String AUTHORIZATION = "authorization";
     public static final String UID = "uid";
     public static final String ROLE = "role";
     // public static final String
